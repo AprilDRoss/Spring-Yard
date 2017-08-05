b0707f3e69c4c2835e7ced97ba7fedfef7bb8965
April Ross <aprilross@Aprils-MacBook-Pro.local>
2017-08-05 15:35:07 -0400

    morerevisions

--- src/main/java/com/example/customer/controller/CustomerController.java
+++ src/main/java/com/example/customer/controller/CustomerController.java
@@ -4,37 +4,55 @@ import com.example.customer.model.Customer;
 import com.example.customer.service.CustomerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
 
-@RestController
+@Controller
 
 public class CustomerController {
 
     @Autowired
-    private CustomerService customerService;
+    CustomerService customerService;
 
     //Json to Java
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @GetMapping("/customers")
+    @RequestMapping("/home")
+    public String getHome(){
+        return "home";
+    }
+
+    @RequestMapping("/customers")
     public List<Customer> getAllCustomers(Model model) {
+
         return customerService.getAllCustomers();
     }
 
-    @PostMapping(value = "/addcustomers")
-    public void addCustomer(@RequestBody Customer customer){
+    @RequestMapping("/addcustomers")
+    public String addCustomers(@RequestParam(value="firstName")String firstName,
+                              @RequestParam(value="lastName")String lastName,
+                              @RequestParam(value="phone") String phone,
+                              @RequestParam(value="email")String email,
+                              Model model){
+        Customer customer = new Customer();
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setPhone(phone);
+        customer.setEmail(email);
         customerService.addCustomer(customer);
-        return "addcustomers";
+        return "redirect: /viewcustomers";
     }
 
-
-
-
-
+    @RequestMapping("/viewcustomers/{id}")
+    public String getCustomerById(@PathVariable int id, Model model){
+        Customer customer = customerService.getCustomerById(id);
+        model.addAttribute("customers", customer);
+        return "viewcustomers";
+    }
 
 }
 
